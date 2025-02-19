a9b20a923f6ae73b3a99965031a565030f977e53
tensei-slime <Onemoment1234567@outlook.com>
2025-02-19 21:51:38 +0530

    Updated 💌

--- base-install.sh
+++ base-install.sh
@@ -8,6 +8,6 @@
 #[ I DO NOT CLAIM THAT THIS IS MINE]
 
 #Clone his repo
-git clone https://github.com/JaKooLit/Arch-Hyprland.git
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git
 cd Arch-Hyprland/
 ./install.sh
